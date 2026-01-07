9152c1851b8e7282625f70a949526cdac6b8c19f
mafiapks <59379921+mafiapks@users.noreply.github.com>
2026-01-06 23:47:42 -0300

    Publish application

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Features from './components/Features';
+import Benefits from './components/Benefits';
+import HowItWorks from './components/HowItWorks';
+import CTA from './components/CTA';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-white">
+      <Navbar />
+      <Hero />
+      <Features />
+      <Benefits />
+      <HowItWorks />
+      <CTA />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
